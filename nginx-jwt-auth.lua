c2fc6917e1f9b131744c395ad8ac624c91416e4d
emrah <emrah.eryilmaz@nordeck.net>
2023-07-23 13:10:29 +0300

    feat: initialize lua module

---
+++ nginx-jwt-auth.lua
@@ -0,0 +1,108 @@
+local cjson_safe  = require 'cjson.safe'
+local basexx = require 'basexx'
+local digest = require 'openssl.digest'
+local hmac = require 'openssl.hmac'
+local pkey = require 'openssl.pkey'
+local algo = "HS256"
+
+-- -----------------------------------------------------------------------------
+function unauthorized()
+  ngx.exit(ngx.HTTP_UNAUTHORIZED)
+end
+
+-- -----------------------------------------------------------------------------
+local algoSign = {
+  ['HS256'] = function(data, key)
+	        return hmac.new(key, 'sha256'):final(data)
+	      end,
+  ['HS384'] = function(data, key)
+	        return hmac.new(key, 'sha384'):final(data)
+	      end,
+  ['HS512'] = function(data, key)
+	        return hmac.new(key, 'sha512'):final(data)
+	      end
+}
+
+-- -----------------------------------------------------------------------------
+local algoVerify = {
+  ['HS256'] = function(data, sign, key)
+                return sign == algoSign['HS256'](data, key)
+              end,
+  ['HS384'] = function(data, sign, key)
+                return sign == algoSign['HS384'](data, key)
+              end,
+  ['HS512'] = function(data, sign, key)
+                return sign == algoSign['HS512'](data, key)
+              end
+}
+
+-- -----------------------------------------------------------------------------
+function splitToken(token)
+  local segments={}
+  for seg in string.gmatch(token, "[^.]+") do
+    table.insert(segments, seg)
+  end
+
+  return segments
+end
+
+-- -----------------------------------------------------------------------------
+function parseToken(token)
+  local segments = splitToken(token)
+  if #segments ~= 3 then unauthorized() end
+
+  local header, err = cjson_safe.decode(basexx.from_url64(segments[1]))
+  if err then unauthorized() end
+
+  local payload, err = cjson_safe.decode(basexx.from_url64(segments[2]))
+  if err then unauthorized() end
+
+  local sign, err = basexx.from_url64(segments[3])
+  if err then unauthorized() end
+
+  local data = segments[1] .. "." .. segments[2]
+
+  return header, payload, sign, data
+end
+
+-- -----------------------------------------------------------------------------
+function verify(token, algo, key)
+  local header, payload, sign, data = parseToken(token)
+
+  if not header.typ or header.typ ~= "JWT" then unauthorized() end
+  if not header.alg or header.alg ~= algo then unauthorized() end
+  if not algoVerify[algo](data, sign, key) then unauthorized() end
+
+  return true
+end
+
+-- -----------------------------------------------------------------------------
+if not (ngx.var.jwt_key or ngx.var.jwt_key_file) then
+  ngx.log(ngx.ERR, "JWT key is not found")
+  unauthorized()
+end
+
+local key = ngx.var.jwt_key
+
+if ngx.var.jwt_algo then
+  algo = ngx.var.jwt_algo
+end
+
+if not algoVerify[algo] then
+  ngx.log(ngx.ERR, "JWT algoritm is not supported")
+  unauthorized()
+end
+
+local headers = ngx.req.get_headers()
+local auth = headers["authorization"]
+if (not auth) then unauthorized() end
+if (not string.match(auth, "^Bearer ")) then unauthorized() end
+
+local token = string.sub(auth, 8)
+if (not token) then unauthorized() end
+if (not verify(token, algo, key)) then unauthorized() end
+
+ngx.say(ngx.var.jwt_key)
+ngx.say(ngx.var.jwt_key_file)
+ngx.say(algo)
+ngx.say(token)
